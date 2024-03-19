0b6ce9679448bd295ebf7bceb8f0e586f5fb2d00
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-03-19 17:33:28 -0300

    Formularios con Ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
+<template>
+    <h1>Refs</h1>
+    <hr />
+
+    <form action="">
+        <input type="text" v-model="nombre">
+        <select v-model="nivel" name="nivel">
+            <option value="" disabled>Seleccioina un nivel</option>
+            <option value="Basico">Basico</option>
+            <option value="Intermedio">Intermedio</option>
+            <option value="Avanzado">Avanzado</option>
+        </select>
+    </form>
+
+    <h3>{{ nombre }} -  {{ nivel }}</h3>
+
+    {{ msg }}
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// Compostion API
+import { ref } from 'vue'
+
+const nombre = ref("Vue.js")
+const nivel = ref("")
+
+
+// Option API
+// export default {
+    // data: () => ({
+    //     msg: "uaslf",
+    // })
+// }
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>
